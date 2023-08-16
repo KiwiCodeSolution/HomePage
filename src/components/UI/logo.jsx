65847a75da8dd37457cf4c43790a5df38484f5ca
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2023-08-16 17:59:19 +0300

    images

--- src/components/UI/logo.jsx
+++ src/components/UI/logo.jsx
@@ -1,11 +1,7 @@
+import logo from "../../img/logo.png";
+
 const Logo = () => {
-  return (
-    <img
-      src="../../img/logo.png"
-      alt="Logo"
-      className="w-[172px] h-[88px] md:w-[115px] md:h-[59px] xl:w-[172px] xl:h-[88px]"
-    />
-  );
+  return <img src={logo} alt="Logo" className="w-[172px] h-[88px] md:w-[115px] md:h-[59px] xl:w-[172px] xl:h-[88px]" />;
 };
 
 export default Logo;
--- src/sections/howItWorks.jsx
+++ src/sections/howItWorks.jsx
@@ -1,30 +1,34 @@
 import { useState } from "react";
 import Title from "../components/UI/title";
+import how_1 from "../img/how-works-1.png";
+import how_2 from "../img/how-works-2.png";
+import how_3 from "../img/how-works-3.png";
+import how_4 from "../img/how-works-4.png";
 
 const TEXTS = [
   {
     id: "1",
     title: "DESIGN",
     text: "This is the starting point and the most important part because it helps to visualize the whole future website and all its features. Everything can be thought over to the smallest detail of how everything will be working, the website logic, and the final structure. Hence, this is the best way to understand what do you want to get, and you will be able to control every corner of the development, which means not a single little thing will go unnoticed.",
-    img: "/img/how-works-1.png",
+    img: `${how_1}`,
   },
   {
     id: "2",
     title: "HTML CODING",
     text: "This step is needed to see how your website is working without adding backend logic. It displays to us how the animations work and shows all the visual issues. We test adaptability and appearance for different devices.HTML coding not only a technical part but a visualization of your thoughts and ideas on a working website.",
-    img: "/img/how-works-2.png",
+    img: `${how_2}`,
   },
   {
     id: "3",
     title: "BACK-END CODING",
     text: "The back-end is the inner world of your site.Automation, the ability to edit and manage your site yourself. All this can be done for your convenience at this stage.Want to change a picture or add a new post? You don’t need to look for developers for this, now you can do everything yourself. Moreover, exist many comfortable plugins for every case, like SEO plugin where just a few clicks can bring your site up, in search. And the most important part that a well-written back-end makes your site work quickly and efficiently.",
-    img: "/img/how-works-3.png",
+    img: `${how_3}`,
   },
   {
     id: "4",
     title: "START YOUR PROJECT",
     text: "Everything is ready, we did it the way you want, and now we can transfer the site to your hosting. The business is working and developing, you have an excellent website that fully displays all your capabilities and takes you to a completely new level.",
-    img: "/img/how-works-4.png",
+    img: `${how_4}`,
   },
 ];
 
