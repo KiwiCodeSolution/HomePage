3c59249679833bf6b0dc21141f2555e9ed9b44bd
Evgenija-P <e.a.poduzova@gmail.com>
2023-08-17 22:11:49 +0300

    language

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
 import * as icons from "../icons/iconComponent";
 import Circle from "./UI/circle";
@@ -8,24 +9,20 @@ const SERVICES = [
   {
     id: "1",
     Icon: icons.Design,
-    title: "UI / UX Design",
-    text: "The process of developing UI/UX design for websites includes analyzing the target audience and their needs, creating ideas and design concepts, developing prototypes and testing them for user experience, as well as  defining the color palette, fonts, and other design elements.",
   },
   {
     id: "2",
     Icon: icons.Marketing,
-    title: "Omnichannel Marketing",
-    text: "Omnichannel marketing is a marketing strategy that involves using various communication channels withcustomers, such as email, social media, mobile apps, SMS messages, and others, to create a seamless and consistent experience of interacting with a brand regardless of the customer's chosen communication channel.",
   },
   {
     id: "3",
     Icon: icons.Application,
-    title: "Desktop Application Development",
-    text: "Desktop application development is the process of creating software to run on personal computers, which are installed and executed without internet access, and can have various functionalities ranging from processing text and tables to advanced graphical capabilities and use of databases.",
   },
 ];
 
 const ServiсesList = ({ type }) => {
+  const { t, i18n } = useTranslation();
+
   const textAnimation = {
     hidden: {
       y: 100,
@@ -39,7 +36,7 @@ const ServiсesList = ({ type }) => {
       className="grid grid-cols-1 md:grid-cols-2 gap-y-10 md:gap-y-4 xl:gap-y-0 xl:grid-cols-3 md:gap-x-[30px] xl:gap-x-[90px] justify-between mb-[60px] xl:mb-[76px] relative"
       id="services"
     >
-      {SERVICES.map(({ id, Icon, title, text }) => (
+      {SERVICES.map(({ id, Icon }) => (
         <motion.li
           key={id}
           className={`flex flex-col max-w-[354px]`}
@@ -53,8 +50,10 @@ const ServiсesList = ({ type }) => {
             <Icon className={"absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"} />
           </Circle>
           <icons.Line />
-          <p className="mt-1 mb-2 text-2xl font-medium leading-[1.4]">{title}</p>
-          <p className="text-base leading-[1.4] opacity-[0.6] text-justify xl:text-left">{text}</p>
+          <p className="mt-1 mb-2 text-2xl font-medium leading-[1.4]">{t(`services.${id - 1}.title`)}</p>
+          <p className="text-base leading-[1.4] opacity-[0.6] text-justify xl:text-left">
+            {t(`services.${id - 1}.text`)}
+          </p>
         </motion.li>
       ))}
     </ul>
@@ -62,7 +61,7 @@ const ServiсesList = ({ type }) => {
     <ul className="flex flex-col gap-y-4 max-w-[245px]">
       {SERVICES.map(({ id, title }) => (
         <li key={id} className="text-2xl font-medium">
-          {title}
+          {t(`services.${id - 1}.title`)}
         </li>
       ))}
     </ul>
--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,6 @@
 import i18next from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 
-i18next
-  .use(Backend) // or any other backend implementation
-  .use(LanguageDetector) // or any other implementation
-
-  .init({ fallbackLng: "en" });
+i18next.use(Backend).use(LanguageDetector).use(initReactI18next).init({ debug: true, fallbackLng: "en" });
